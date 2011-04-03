50d9a8b575ed284ed70b1ae7701d1eac9bb68a54
Gabe Gorelick <gabegorelick@gmail.com>
2011-04-03 14:38:03 -0400

    Smarter waiting for GUI application

--- src/edu/umd/cs/guitar/model/SWTApplication.java
+++ src/edu/umd/cs/guitar/model/SWTApplication.java
@@ -51,6 +51,12 @@ import edu.umd.cs.guitar.util.GUITARLog;
  */
 public class SWTApplication extends GApplication {
 
+	/**
+	 * Default maximum time (in milliseconds) to wait for the application under
+	 * test to start. This can be overridden with {@link #setInitialDelay(int)}.
+	 */
+	public static final int DEFAULT_INITIAL_DELAY = 5000;
+	
 	private String mainClassName;
 	private Thread appThread;
 	
@@ -60,11 +66,20 @@ public class SWTApplication extends GApplication {
 	
 	private int initialDelay;
 	private Set<URL> urls;
-	
+		
+	/**
+	 * 
+	 * @param mainClassName
+	 *            name of class that contains the GUI's <code>main</code> method
+	 * @param appThread
+	 *            thread the GUI is running on (usually the <code>main</code>
+	 *            thread)
+	 */
 	public SWTApplication(String mainClassName, Thread appThread) {
 		this.mainClassName = mainClassName;
 		this.appThread = appThread;
-		
+		this.initialDelay = DEFAULT_INITIAL_DELAY;
+			
 		urls = new HashSet<URL>();
 
 		// System URLs
@@ -112,44 +127,68 @@ public class SWTApplication extends GApplication {
 	public void connect() {
 		connect(null);
 	}
-	
+
 	/**
+	 * <p>
 	 * Wait for SWT application to start. In other GUITAR plugins, this method
-	 * starts the GUI application. But in SWT GUITAR, the application has 
-	 * already been started by the time this method is called. Thus, this 
-	 * method only waits for the application to be ready for ripping. 
+	 * starts the GUI application. But in SWT GUITAR, the application has
+	 * already been started by the time this method is called. Thus, this method
+	 * only waits for the application to be ready for ripping.
+	 * <p/>
+	 * <p>
+	 * Ideally, this method would wait until it is notified that the application
+	 * under test has started. But starting the application blocks, so no
+	 * notification can be sent. Thus, this method has to simply
+	 * {@link Thread#sleep(long) sleep} until the application is ready.
+	 * </p>
 	 * 
-	 * @param args - usually the arguments to the GUI main class, but since
-	 * the GUI is already started by the time this method is called, this
-	 * argument is ignored
+	 * @param args
+	 *            usually the arguments to the GUI main class, but since the GUI
+	 *            is already started by the time this method is called, this
+	 *            argument is ignored
+	 * 
+	 * @throws ApplicationConnectException
+	 *             thrown if application did not start within the {@link #setMaxWait(int) max wait time} or if 
+	 *             sleeping failed
 	 */
 	@Override
 	public void connect(String[] args) throws ApplicationConnectException {
 		try {
-			// sleep because user said so
-			Thread.sleep(initialDelay);
-			
 			// sleep because we have to
-			int ms = 2000;
-			System.out.println("Waiting for GUI to initialize for: " + ms + "ms");
-			Thread.sleep(ms); // TODO wait for event from Display instead of sleeping
-			onGuiStarted();
+			int sleepIncrement = 100;
+			int totalSleepTime = 0;
+			
+			while ((guiDisplay = Display.findDisplay(appThread)) == null) {
+				GUITARLog.log.debug("GUI not ready yet");				
+				if (totalSleepTime > initialDelay) {
+					GUITARLog.log.error("Timed out waiting for GUI to start");
+					throw new ApplicationConnectException();
+				}
+				GUITARLog.log.debug("Waiting for GUI to initialize for: " + sleepIncrement + "ms");
+				Thread.sleep(sleepIncrement);
+				totalSleepTime += sleepIncrement;
+			}
+			
+			// make sure display is not only non-null, but ready
+			Thread.sleep(sleepIncrement);
+			
+			// force update of display, just to be sure
+			// also good since syncExec waits for "reasonable opportunity"
+			guiDisplay.syncExec(new Runnable() {
+				@Override
+				public void run() {
+					guiDisplay.update();
+					GUITARLog.log.debug("Display ready");
+				}
+			});
 		} catch (InterruptedException e) {
 			// doesn't support causes :(
+			GUITARLog.log.error("connect encountered InterruptedException, " +
+					"throwing ApplicationConnectException");
 			throw new ApplicationConnectException();
 		}
 	}
 	
-	/**
-	 * This method is called once the GUI has been started. Subclasses should
-	 * still call this method when overriding, as this method does needed set
-	 * up.
-	 */
-	protected void onGuiStarted() {
-		// set display now that there is one
-		guiDisplay = Display.findDisplay(appThread);
-	}
-
 	/**
 	 * Start the application under test. This method simply invokes the 
 	 * application's main method with the arguments specified in the 
@@ -176,19 +215,24 @@ public class SWTApplication extends GApplication {
 		try {
 			Class<?> clazz = Class.forName(mainClassName, true, loader);
 			mainMethod = clazz.getMethod("main", new Class[] { String[].class });
-			mainMethod.invoke(null, new Object[] { argsToApp });
 		} catch (ClassNotFoundException e) {
 			// all these catch blocks make me want Java 7
 			throw new SWTApplicationStartException(e);
 		} catch (IllegalArgumentException e) {
 			throw new SWTApplicationStartException(e);
-		} catch (IllegalAccessException e) {
-			throw new SWTApplicationStartException(e);
-		} catch (InvocationTargetException e) {
-			throw new SWTApplicationStartException(e);
 		} catch (SecurityException e) {
 			throw new SWTApplicationStartException(e);
 		} catch (NoSuchMethodException e) {
+			throw new SWTApplicationStartException(e);
+		} 
+		
+		try {
+			mainMethod.invoke(null, new Object[] { argsToApp });
+		} catch (IllegalArgumentException e) {
+			throw new SWTApplicationStartException(e);
+		} catch (IllegalAccessException e) {
+			throw new SWTApplicationStartException(e);
+		} catch (InvocationTargetException e) {
 			throw new SWTApplicationStartException(e);
 		}
 	}
@@ -243,8 +287,15 @@ public class SWTApplication extends GApplication {
 		return initialDelay;
 	}
 	
+	/**
+	 * Set the maximum amount of time to wait until the GUI is ready. By
+	 * default, this is {@link #DEFAULT_INITIAL_DELAY}.
+	 * 
+	 * @param delay
+	 *            time to wait in milliseconds
+	 */
 	public void setInitialDelay(int delay) {
 		initialDelay = delay;
 	}
-
+	
 }

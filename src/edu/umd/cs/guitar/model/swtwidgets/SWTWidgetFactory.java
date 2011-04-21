93fd078557221883d9d0f23e2e4ed5f1ab1b55be
Gabe Gorelick <gabegorelick@gmail.com>
2011-04-20 20:29:26 -0400

    SWTWidgetFactory: document everything

--- src/edu/umd/cs/guitar/model/swtwidgets/SWTWidgetFactory.java
+++ src/edu/umd/cs/guitar/model/swtwidgets/SWTWidgetFactory.java
@@ -30,19 +30,72 @@ import edu.umd.cs.guitar.util.GUITARLog;
 
 /**
  * <p>
- * <code>SWTWidgetFactory</code> handles the wrapping of SWT widgets into their
- * appropriate SWTGuitar type. TODO finish documenting
+ * {@code SWTWidgetFactory} handles the adapting of {@link Widget Widgets} onto
+ * GUITAR types, specifically {@link SWTWidget SWTWidgets}. Clients can register
+ * widget adapters that wrap {@link Widget Widgets} in
+ * {@code SWTWidget SWTWidgets}). {@link SWTWidgetFactory} automatically
+ * registers the default widget adapters located in this package. These adapters
+ * can be overriden with the {@link #registerWidgetAdapter(Class, Class)
+ * registerWidgetAdapter} method. For example, the following code will override
+ * the default adapter ({@link SWTTree}) for SWT's {@link Tree} type:
+ * 
+ * <pre>
+ * SWTWidgetFactory.INSTANCE.registerWidgetAdapter(Tree.class, FOO.class);
+ * </pre>
+ * 
+ * </p>
+ * <p>
+ * There are some limitations to widget adapters: they must be a subtype of
+ * {@code SWTWidget}, and they must have an accessible
+ * {@link Class#getDeclaredConstructor(Class...) declared constructor} that
+ * accepts two parameters. The first parameter must be the type of the
+ * {@code Widget} that it is registered to wrap, and the second parameter must
+ * be of type {@link SWTWindow}. For example, if we want a new adapter named
+ * {@code SWTShell} to wrap an SWT {@code Shell} it would need to have a
+ * constructor with the following signature:
+ * 
+ * <pre>
+ * public SWTShell(Shell, SWTWindow)
+ * </pre>
+ * 
+ * </p>
+ * 
+ * <p>
+ * When selecting an appropriate {@code SWTWidget} subtype for a given
+ * {@code Widget}, {@code SWTWidgetFactory} attempts to find the most relevant
+ * registered adapter. For example, if we have both a {@link Composite} adapter
+ * and a {@link Control} adapter registered, a call to
+ * {@link #getWidgetAdapter(Widget) getWidgetAdapter(Shell)} will return the
+ * {@code Composite} adapter, since that is the closer type to {@code Shell}
+ * (since {@code Shell} is a descendant of {@code Composite} and
+ * {@code Composite} is a descendant of {@code Control}).
+ * </p>
+ * <p>
+ * An instance of this class can be obtained through its singleton
+ * {@link #INSTANCE} member. See Joshua Bloch's <i>Effective Java</i>, the
+ * relevant portions of which are reproduced <a
+ * href="http://drdobbs.com/java/208403883?pgno=3">here</a>, for why this
+ * singleton is implemented as an enum.
+ * </p>
+ * <p>
+ * All methods of this class are thread safe.
+ * </p>
  * 
  * @author Gabe Gorelick
  * 
  */
 public enum SWTWidgetFactory {
 
+	/**
+	 * The singleton instance of this class.
+	 */
 	INSTANCE;
 		
 	private final Map<Class<? extends Widget>, Class<? extends SWTWidget>> widgetAdapters;
 	
 	private SWTWidgetFactory() {
+		// thread safety VERY important since there's only one instance of this
+		// class that's shared among all threads
 		widgetAdapters = Collections.synchronizedMap(new HashMap<Class<? extends Widget>, Class<? extends SWTWidget>>());
 		registerDefaultAdapters();
 	}
@@ -75,11 +128,24 @@ public enum SWTWidgetFactory {
 		registerWidgetAdapter(TabItem.class, SWTTabItem.class);
 		registerWidgetAdapter(TreeItem.class, SWTTreeItem.class);
 	}
-	
+
 	/**
-	 * Register a widget adapter. 
+	 * <p>
+	 * Register a widget adapter. The adapter must have an accessible declared
+	 * constructor that accepts two parameters. The first parameter must be of
+	 * type {@code widgetType}, and the second parameter must be of type
+	 * {@link SWTWindow}. See the class documentation for more information.
+	 * </p>
+	 * <p>
+	 * This method, like all methods of this class, is thread safe, since
+	 * registered adapters are stored in a
+	 * {@link Collections#synchronizedMap(Map) synchronized Map}.
+	 * </p>
+	 * 
 	 * @param widgetType
+	 *            type of widget this adapter will wrap
 	 * @param adapterType
+	 *            type of adapter
 	 * 
 	 * @see Map#put(Object, Object)
 	 */
@@ -99,6 +165,7 @@ public enum SWTWidgetFactory {
 			}
 		}
 		
+		// sort by type hierarchy
 		Collections.sort(validTypes, new Comparator<Entry<Class<? extends Widget>, Class<? extends SWTWidget>>>() {
 			@Override
 			public int compare(
@@ -123,11 +190,31 @@ public enum SWTWidgetFactory {
 		// return lowest class in type hierarchy
 		return validTypes.get(0);
 	}
-	
+
+	/**
+	 * Get the most appropriate widget adapter for the given widget. This is the
+	 * adapter that is registered to the closest type that the given widget is
+	 * assignable to. See the class documentation for more information.
+	 * 
+	 * @param widget
+	 *            widget to wrap
+	 * @return the most appropriate widget adapter type
+	 */
 	public Class<? extends SWTWidget> getWidgetAdapter(Widget widget) {
 		return getWidgetAdapterEntry(widget).getValue();
 	}
-	
+
+	/**
+	 * Create a new {@code SWTWidget} for a given widget using the registered
+	 * widget adapters. The actual type of the returned object will be the type
+	 * returned by {@link #getWidgetAdapter(Widget) getWidgetAdapter(widget)}.
+	 * 
+	 * @param widget
+	 *            widget to wrap
+	 * @param window
+	 *            window to pass to the adapter's constructor
+	 * @return a widget adapter
+	 */
 	public SWTWidget newSWTWidget(Widget widget, SWTWindow window) {
 		Entry<Class<? extends Widget>, Class<? extends SWTWidget>> entry = getWidgetAdapterEntry(widget);
 		Class<? extends Widget> widgetType = entry.getKey();

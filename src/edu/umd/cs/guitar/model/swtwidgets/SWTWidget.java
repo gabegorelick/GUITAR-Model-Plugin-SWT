161834d18deb256b985d141f9350aa89b3b873dc
Gabe Gorelick <gabegorelick@gmail.com>
2011-04-27 20:11:10 -0400

    Copyright headers

--- src/edu/umd/cs/guitar/model/swtwidgets/SWTWidget.java
+++ src/edu/umd/cs/guitar/model/swtwidgets/SWTWidget.java
@@ -1,3 +1,22 @@
+/*	
+ *  Copyright (c) 2009-@year@. The GUITAR group at the University of Maryland. Names of owners of this group may
+ *  be obtained by sending an e-mail to atif@cs.umd.edu
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without 
+ *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ *	the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
+ *	conditions:
+ * 
+ *	The above copyright notice and this permission notice shall be included in all copies or substantial 
+ *	portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+ *	LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO 
+ *	EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+ *	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
+ *	THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ */
 /* Copyright (c) 2010
  * Matt Kirn (mattkse@gmail.com) and Alex Loeb (atloeb@gmail.com)
  * 
@@ -135,11 +154,7 @@ public abstract class SWTWidget extends GComponent {
 
 		control[0].getDisplay().syncExec(new Runnable() {
 			@Override
-			public void run() {
-				if (control[0].isDisposed()) {
-					throw new AssertionError("control is disposed");
-				}
-												
+			public void run() {												
 				while (!(control[0] instanceof Shell)) {
 					point[0].x += control[0].getLocation().x;
 					point[0].y += control[0].getLocation().y;
